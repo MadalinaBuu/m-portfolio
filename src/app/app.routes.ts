611b86b5baf17e8eb38fa6a5659365429cc7e95d
MadalinaBuu <madalina.ciucioiu@gmail.com>
2026-03-07 17:41:08 +0200

    navbar + components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { DescriptionComponent } from './description/description.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DescriptionComponent },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'work', component: MyWorkComponent },
+  { path: 'contact', component: ContactComponent },
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  scrolled = false;
+  menuOpen = false;
+  isDark = false;
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled = window.scrollY > 50;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark-theme');
+  }
+}
