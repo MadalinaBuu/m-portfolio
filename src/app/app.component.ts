e17762a7598757d096233a5d893944d984fe5edd
MadalinaBuu <madalina.ciucioiu@gmail.com>
2026-03-28 10:40:41 +0200

    description component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { DescriptionComponent } from "./description/description.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, DescriptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
