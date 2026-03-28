7b28cedcc046501b047360919af654b9d3cb833f
MadalinaBuu <madalina.ciucioiu@gmail.com>
2026-03-28 16:42:35 +0200

    footer + my-work components

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-description',
-  imports: [],
   templateUrl: './description.component.html',
-  styleUrl: './description.component.scss'
+  styleUrls: ['./description.component.scss']
 })
 export class DescriptionComponent {
 
+  constructor(
+    private router: Router,
+    private scroller: ViewportScroller
+  ) {}
+
+  goTo(fragment: string) {
+    this.router.navigate(['/'], { fragment }).then(() => {
+      setTimeout(() => {
+        this.scroller.scrollToAnchor(fragment);
+      }, 50);
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { DescriptionComponent } from "../description/description.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
+import { ContactComponent } from "../contact/contact.component";
+import { MyWorkComponent } from "../my-work/my-work.component";
 
 @Component({
   selector: 'app-home',
-  imports: [DescriptionComponent, AboutMeComponent],
+  imports: [DescriptionComponent, AboutMeComponent, ContactComponent, MyWorkComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
